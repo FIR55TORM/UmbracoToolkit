53adfa82e2ba8fcc5df9080bef3ab6a5edff11a8
FIR55TORM <FIR55TORM@gmail.com>
2014-08-28 00:16:59 +0100

    New Breadcrumb overloads
    Ability to add a root level css class and seperator html if you so
    desire.

--- Umbraco7/HtmlHelpers/BreadcrumbHtmlHelper.cs
+++ Umbraco7/HtmlHelpers/BreadcrumbHtmlHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Umbraco.Core.Models;
@@ -9,10 +10,16 @@ namespace UmbracoToolkit.Umbraco7.HtmlHelpers
 {
     public static class BreadcrumbHtmlHelper
     {
+
+        public static MvcHtmlString BreadcrumbFor(this HtmlHelper helper)
+        {
+            return BreadcrumbFor(helper, "breadcrumb");
+        }
+
         /// <summary>
         /// This Helper is specifically for Umbraco 
         /// </summary>
-        public static MvcHtmlString BreadcrumbFor(this HtmlHelper helper)
+        public static MvcHtmlString BreadcrumbFor(this HtmlHelper helper, string cssClass, string seperatorHtml = "")
         {
             var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
 
@@ -23,22 +30,22 @@ namespace UmbracoToolkit.Umbraco7.HtmlHelpers
             publishedContent.Add(umbracoHelper.AssignedContentItem);//Add current context
             publishedContent.AddRange(umbracoHelper.AssignedContentItem.Ancestors().Where(t => t.IsVisible())); //Add the rest
 
-            return new MvcHtmlString(BuildBreadcrumb(publishedContent));
+            return new MvcHtmlString(BuildBreadcrumb(publishedContent, cssClass, seperatorHtml));
         }
 
-        private static string BuildBreadcrumb(List<IPublishedContent> publishedContent)
+        private static string BuildBreadcrumb(List<IPublishedContent> publishedContent, string cssClass, string seperatorHtml)
         {
-            return BuildCrumbs(publishedContent, publishedContent.Count - 1);
+            return BuildCrumbs(publishedContent, publishedContent.Count - 1, cssClass, seperatorHtml);
         }
 
-        private static string BuildCrumbs(List<IPublishedContent> publishedContent, int index)
+        private static string BuildCrumbs(List<IPublishedContent> publishedContent, int index, string cssClass, string seperatorHtml)
         {
             var content = publishedContent[index];
 
             TagBuilder div = new TagBuilder("div");
 
             if (index == publishedContent.Count - 1)
-                div.AddCssClass("breadcrumb");
+                div.AddCssClass(cssClass);
 
             if (index != publishedContent.Count - 1)
                 div.Attributes.Add("itemprop", "child");
@@ -54,11 +61,14 @@ namespace UmbracoToolkit.Umbraco7.HtmlHelpers
             a.Attributes.Add("itemprop", "url");
             a.Attributes.Add("href", content.Url);
             a.InnerHtml = title.ToString(TagRenderMode.Normal);
+            
+            if (!String.IsNullOrEmpty(seperatorHtml) && index != 0)
+                a.InnerHtml += seperatorHtml;
 
             div.InnerHtml = a.ToString();
 
             if (index != 0)
-                div.InnerHtml += BuildCrumbs(publishedContent, index - 1);
+                div.InnerHtml += BuildCrumbs(publishedContent, index - 1, cssClass, seperatorHtml);
 
             return div.ToString();
         }
